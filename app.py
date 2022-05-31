f60e8743c7198485ed7adcefcc0f3db829339cd5
FYEN45 <ferry.ahogun@gmail.com>
2022-05-31 17:19:01 +0700

    Add Edit Kamar, & Reservasi Kamar Client

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '@#$123456&*()'
 
 # Session Lifetime
-app.permanent_session_lifetime = timedelta(minutes=10)
+app.permanent_session_lifetime = timedelta(minutes=60)
 
 # Koneksi MYSQL (Tanpa SQLAlchemy)
 # Mempersiapkan koneksi dengan server mysql.
@@ -51,12 +51,18 @@ def home():
 def daftarUser():
     if 'nama_user' in session:
         if session['status'] == "ADMIN":
-            cursor = mysql.connection.cursor()
-            cursor.execute('SELECT * FROM user')
-            hasil = cursor.fetchall()
-        
-            cursor.close()
-            return render_template('admin/user/daftarUser.html', container = hasil)
+            try:
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM user')
+                hasil = cursor.fetchall()
+            
+                cursor.close()
+                return render_template('admin/user/daftarUser.html', container = hasil)
+
+            except (MySQLdb.Error) as err:
+            # Menangkap error dan menampilkan pesan gagal
+                flash('Gagal menampilkan daftar user! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/home')
     
     return redirect('/login')
 
@@ -69,12 +75,17 @@ def daftarUser():
 def daftarKamar():
     if 'nama_user' in session:
         if session['status'] == "ADMIN":
-            cursor = mysql.connection.cursor()
-            cursor.execute('SELECT * FROM kamar')
-            hasil = cursor.fetchall()
-        
-            cursor.close()
-            return render_template('admin/kamar/daftarKamar.html', container = hasil)
+            try: 
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM kamar')
+                hasil = cursor.fetchall()
+            
+                cursor.close()
+                return render_template('admin/kamar/daftarKamar.html', container = hasil)
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan menampilkan pesan gagal
+                flash('Gagal menambahkan kamar! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/home')
     
     return redirect('/login')
 
@@ -110,6 +121,66 @@ def tambahKamar():
 
     return redirect('/login')
 
+# EDIT KAMAR - ADMIN
+@app.route('/editKamar/<kodeKamar>', methods = ['GET', 'POST'])
+def editKamar(kodeKamar):
+    if 'nama_user' in session:
+        if session['status'] == "ADMIN":
+            if (request.method == 'POST'):
+                try:
+                    # Mengambil input dari form
+                    kamar = request.form
+                    kode_kamar = kamar['kode_kamar']
+                    nama_kamar = kamar['nama_kamar']
+                    harga_kamar =  kamar['harga_kamar']
+                    jumlah_kamar = kamar['jumlah_kamar']
+                    kamar_tersedia = kamar['kamar_tersedia']
+
+                    # Menghubungkan ke database dan melakukan UPDATE pada tabel buku
+                    cursor = mysql.connection.cursor()
+                    cursor.execute('UPDATE kamar SET nama_kamar=%s, harga_kamar=%s, jumlah_kamar=%s, kamar_tersedia=%s WHERE kode_kamar=%s', (nama_kamar, harga_kamar, jumlah_kamar, kamar_tersedia, kode_kamar))
+                    mysql.connection.commit()
+                    cursor.close()
+
+                    flash('Kamar berhasil diedit!')
+                    return redirect('/daftarKamar')
+                
+                except (MySQLdb.Error) as err:
+                    # Menangkap Error dan memberikan pesan gagal
+                    flash('Buku gagal diedit! %d: %s' % (err.args[0], err.args[1]))
+                    return redirect('/daftarKamar')
+            
+            # Menghubungkan ke database dan mengambil informasi buku yang akan di edit
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT * FROM kamar WHERE kode_kamar=%s', (kodeKamar,))
+            hasil = cursor.fetchall()
+
+            cursor.close()
+            return render_template('admin/kamar/editKamar.html', container = hasil)
+    
+    return redirect('/login')
+
+# HAPUS KAMAR - ADMIN
+@app.route('/hapusKamar/<kodeKamar>', methods = ['GET', 'POST'])
+def hapusKamar(kodeKamar):
+    if 'nama_user' in session:
+        if session['status'] == "ADMIN":
+            try:
+                cursor = mysql.connection.cursor()
+                cursor.execute('DELETE FROM kamar WHERE kode_kamar=%s', (kodeKamar,))
+                mysql.connection.commit()        
+                cursor.close()
+                
+                flash('Kamar berhasil dihapus!')
+                return redirect('/daftarKamar')
+
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Kamar gagal dihapus! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/daftarKamar')
+
+    return redirect('/login')
+
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 # CRUD RESERVASI
 # ------------------------------------------------------------------------------------------------------------------------------------------------------    
@@ -119,20 +190,26 @@ def tambahKamar():
 def daftarReservasi():
     if 'nama_user' in session:
         if session['status'] == "ADMIN":
-            cursor = mysql.connection.cursor()
-            cursor.execute('SELECT * FROM reservasi')
-            reservasi = cursor.fetchall()
+            try:
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM reservasi')
+                reservasi = cursor.fetchall()
 
-            cursor.execute('SELECT * FROM statuscheck')
-            status = cursor.fetchall()
-            cursor.close()
+                cursor.execute('SELECT * FROM statuscheck')
+                status = cursor.fetchall()
+                cursor.close()
 
-            hasil = []
+                hasil = []
 
-            for x in range(len(reservasi)):
-                hasil.append(reservasi[x] + status[x])
+                for x in range(len(reservasi)):
+                    hasil.append(reservasi[x] + status[x])
 
-            return render_template('admin/reservasi/daftarReservasi.html', container = hasil)
+                return render_template('admin/reservasi/daftarReservasi.html', container = hasil)
+            
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal menampilkan daftar reservasi! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/home')
 
     return redirect('/login')
 
@@ -151,79 +228,119 @@ def tambahReservasi():
 
                 return redirect('/konfirmasiReservasi/{}/{}/{}/{}/{}'.format(kodeReservasi, kodeUser, kodeKamar, tglCheckin, jumlahMalam))
 
-            cursor = mysql.connection.cursor()
-            cursor.execute('SELECT * FROM kamar')
-            kamar = cursor.fetchall()
+            try: 
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM kamar')
+                kamar = cursor.fetchall()
 
-            cursor.execute('SELECT * FROM user WHERE status="CLIENT"')
-            user = cursor.fetchall()
-            cursor.close()
+                cursor.execute('SELECT * FROM user WHERE status="CLIENT"')
+                user = cursor.fetchall()
+                cursor.close()
 
-            return render_template('admin/reservasi/tambahReservasi.html', container = [user, kamar])
+                return render_template('admin/reservasi/tambahReservasi.html', container = [user, kamar])
+
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal menampilkan data! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/daftarReservasi')
         
         elif session['status'] == "CLIENT":
-            cursor = mysql.connection.cursor()
-            cursor.execute('SELECT * FROM kamar')
-            kamar = cursor.fetchall()
+            if (request.method == 'POST'):
+                hasil = request.form
+                kodeReservasi = hasil['kode_reservasi']
+                kodeUser = hasil['kode_user']
+                kodeKamar = hasil['kode_kamar']
+                tglCheckin = hasil['tglCheckin']
+                jumlahMalam = hasil['jumlah_malam']
 
-            cursor.execute('SELECT * FROM user WHERE kode_user="{{ session["kode_user"] }}"')
-            user = cursor.fetchall()
-            cursor.close()
+                return redirect('/konfirmasiReservasi/{}/{}/{}/{}/{}'.format(kodeReservasi, kodeUser, kodeKamar, tglCheckin, jumlahMalam))
+
+            try: 
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM kamar')
+                kamar = cursor.fetchall()
+
+                cursor.execute('SELECT * FROM user WHERE kode_user="%s"', (session['kode_user'],))
+                user = cursor.fetchall()
+                cursor.close()
+
+                return render_template('client/reservasi/tambahReservasi.html', container = [user, kamar])
+            
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal menampilkan data! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/home')
     
     return redirect('/login')
 
-# KONFIRMASI RESERVASI - ADMIN
+# KONFIRMASI RESERVASI - ADMIN & CLIET
 @app.route('/konfirmasiReservasi/<kodeReservasi>/<kodeUser>/<kodeKamar>/<tglCheckin>/<jumlahMalam>', methods=['GET', 'POST'])
 def konfirmasiReservasi(kodeReservasi, kodeUser, kodeKamar, tglCheckin, jumlahMalam):
-    if (request.method == "POST"):
-        hasil = request.form
-        kodeReservasi = hasil['kode_reservasi']
-        kodeUser = hasil['kode_user']
-        kodeKamar = hasil['kode_kamar']
-        tglCheckin = hasil['tglCheckin']
-        jumlahMalam = hasil['jumlahMalam']
-        tglCheckout = hasil['tglCheckout']
-        totalBiaya = hasil['totalBiaya']
+    if 'nama_user' in session:
+        if (request.method == "POST"):
+            hasil = request.form
+            kodeReservasi = hasil['kode_reservasi']
+            kodeUser = hasil['kode_user']
+            kodeKamar = hasil['kode_kamar']
+            tglCheckin = hasil['tglCheckin']
+            jumlahMalam = hasil['jumlahMalam']
+            tglCheckout = hasil['tglCheckout']
+            totalBiaya = hasil['totalBiaya']
+
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT * FROM kamar WHERE kode_kamar="{}"'.format(kodeKamar))
+            kamar = cursor.fetchall()
+            cursor.close()
+
+            if (int(kamar[0][4]) >= 1):
+                try:
+                    cursor = mysql.connection.cursor()
+                    cursor.execute('INSERT INTO reservasi(kode_reservasi, kode_user, kode_kamar, tgl_checkin, tgl_checkout, jumlah_malam, total_biaya) VALUES(%s, %s, %s, %s, %s, %s, %s)', (kodeReservasi, kodeUser, kodeKamar, tglCheckin, tglCheckout, jumlahMalam, totalBiaya))
+                    cursor.execute('INSERT INTO statuscheck(kode_reservasi, isCheckin, isCheckout) VALUES(%s, %s, %s)', (kodeReservasi, "NO", "NO"))
+                    cursor.execute('UPDATE kamar SET kamar_tersedia=%s WHERE kode_kamar=%s', (int(kamar[0][4] - 1), kamar[0][0]))
+                    mysql.connection.commit()
+                    cursor.close()
+                    flash('Berhasil melakukan reservasi!')
+
+                except (MySQLdb.Error) as err:
+                    # Menangkap error dan memberikan pesan gagal
+                    flash('Gagal melakukan reservasi! %d: %s' % (err.args[0], err.args[1]))
+                    return redirect('/home')
+
+            else:
+                flash('Gagal melakukan Reservasi, kamar tidak tersedia!')
+                        
+            return redirect('/home')
 
         cursor = mysql.connection.cursor()
         cursor.execute('SELECT * FROM kamar WHERE kode_kamar="{}"'.format(kodeKamar))
         kamar = cursor.fetchall()
         cursor.close()
 
-        if (int(kamar[0][4]) >= 1):
-            cursor = mysql.connection.cursor()
-            cursor.execute('INSERT INTO reservasi(kode_reservasi, kode_user, kode_kamar, tgl_checkin, tgl_checkout, jumlah_malam, total_biaya) VALUES(%s, %s, %s, %s, %s, %s, %s)', (kodeReservasi, kodeUser, kodeKamar, tglCheckin, tglCheckout, jumlahMalam, totalBiaya))
-            cursor.execute('INSERT INTO statuscheck(kode_reservasi, isCheckin, isCheckout) VALUES(%s, %s, %s)', (kodeReservasi, "NO", "NO"))
-            cursor.execute('UPDATE kamar SET kamar_tersedia=%s WHERE kode_kamar=%s', (int(kamar[0][4] - 1), kamar[0][0]))
-            mysql.connection.commit()
-            cursor.close()  
-        else:
-            flash('Gagal melakukan Reservasi, tidak ada kamar tersedia!')
-                    
-        flash('Berhasil menambahkan kamar!')
-        return redirect('/daftarReservasi')
+        hargaMalam = kamar[0][2]
 
-    cursor = mysql.connection.cursor()
-    cursor.execute('SELECT * FROM kamar WHERE kode_kamar="{}"'.format(kodeKamar))
-    kamar = cursor.fetchall()
-    cursor.close()
-
-    hargaMalam = kamar[0][2]
-
-    data = [kodeReservasi, kodeUser, kodeKamar, tglCheckin, jumlahMalam, hargaMalam]
-    return render_template('admin/reservasi/konfirmasiReservasi.html', container = data)
+        data = [kodeReservasi, kodeUser, kodeKamar, tglCheckin, jumlahMalam, hargaMalam]
+        return render_template('admin/reservasi/konfirmasiReservasi.html', container = data)
+    
+    return redirect('/login')
 
 # HAPUS RESERVASI - ADMIN
 @app.route('/hapusReservasi/<kodeReservasi>', methods=['GET', 'POST'])
 def hapusReservasi(kodeReservasi):   
     if 'nama_user' in session:
         if session['status'] == "ADMIN":
-            cursor = mysql.connection.cursor()
-            cursor.execute('DELETE FROM statuscheck WHERE kode_reservasi=%s', (kodeReservasi,))
-            cursor.execute('DELETE FROM reservasi WHERE kode_reservasi=%s', (kodeReservasi,))
-            mysql.connection.commit()
-            cursor.close()
+            try: 
+                cursor = mysql.connection.cursor()
+                cursor.execute('DELETE FROM statuscheck WHERE kode_reservasi=%s', (kodeReservasi,))
+                cursor.execute('DELETE FROM reservasi WHERE kode_reservasi=%s', (kodeReservasi,))
+                mysql.connection.commit()
+                cursor.close()
+                flash('Berhasil menghapus reservasi!')
 
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal menghapus reservasi! %d: %s' % (err.args[0], err.args[1]))
+            
             return redirect('/daftarReservasi')
 
     return redirect('/login')
@@ -233,9 +350,14 @@ def hapusReservasi(kodeReservasi):
 def checkin(kodeReservasi):   
     if 'nama_user' in session:
         if session['status'] == "ADMIN":
-            cursor = mysql.connection.cursor()
-            cursor.execute('UPDATE statuscheck SET isCheckin=%s WHERE kode_reservasi=%s', ("YES", kodeReservasi))
-            mysql.connection.commit()
+            try :
+                cursor = mysql.connection.cursor()
+                cursor.execute('UPDATE statuscheck SET isCheckin=%s WHERE kode_reservasi=%s', ("YES", kodeReservasi))
+                mysql.connection.commit()
+
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal checkin! %d: %s' % (err.args[0], err.args[1]))
 
             return redirect('/daftarReservasi')
 
@@ -246,16 +368,20 @@ def checkin(kodeReservasi):
 def checkout(kodeReservasi, kodeKamar):   
     if 'nama_user' in session:
         if session['status'] == "ADMIN":
+            try:
+                cursor = mysql.connection.cursor()
+                cursor.execute('SELECT * FROM kamar WHERE kode_kamar="{}"'.format(kodeKamar))
+                kamar = cursor.fetchall()
+        
+                cursor.execute('UPDATE statuscheck SET isCheckout=%s WHERE kode_reservasi=%s', ("YES", kodeReservasi))
+                cursor.execute('UPDATE kamar SET kamar_tersedia=%s WHERE kode_kamar=%s', (int(kamar[0][4] + 1), kamar[0][0]))
 
-            cursor = mysql.connection.cursor()
-            cursor.execute('SELECT * FROM kamar WHERE kode_kamar="{}"'.format(kodeKamar))
-            kamar = cursor.fetchall()
-    
-            cursor.execute('UPDATE statuscheck SET isCheckout=%s WHERE kode_reservasi=%s', ("YES", kodeReservasi))
-            cursor.execute('UPDATE kamar SET kamar_tersedia=%s WHERE kode_kamar=%s', (int(kamar[0][4] + 1), kamar[0][0]))
-
-            mysql.connection.commit()
-            cursor.close()
+                mysql.connection.commit()
+                cursor.close()
+            
+            except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal checkout! %d: %s' % (err.args[0], err.args[1]))
 
             return redirect('/daftarReservasi')
 
@@ -276,10 +402,16 @@ def login():
         password = datalogin['password']
 
         # Melakukan SELECT untuk memeriksa apakah username dan password ada dalam database
-        cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM user WHERE email=%s AND password=%s', (email, password))
-        hasil = cursor.fetchall()
-        
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT * FROM user WHERE email=%s AND password=%s', (email, password))
+            hasil = cursor.fetchall()
+
+        except (MySQLdb.Error) as err:
+                # Menangkap error dan memberikan pesan gagal
+                flash('Gagal checkin! %d: %s' % (err.args[0], err.args[1]))
+                return redirect('/login')
+            
         if (len(hasil) > 0):
             # Apabila terdapat data username dan password, verifikasi berhasil
             
